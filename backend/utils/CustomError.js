76545736d45979a18ab647b554d6ecbd7366f51a
Shreshth <shreshth100vishwa@gmail.com>
2025-03-30 19:06:15 +0530

    Removed customError.js file

--- backend/utils/CustomError.js
+++
@@ -1,9 +0,0 @@
-class CustomError extends Error {
-  constructor(statusCode, message) {
-    super();
-    this.statusCode = statusCode;
-    this.message = message;
-  }
-}
-
-export default CustomError;

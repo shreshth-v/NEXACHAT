4e676988f2ed8c58340bc860fbb97dee30ce8007
Shreshth <shreshth100vishwa@gmail.com>
2025-03-30 19:57:25 +0530

    Updated frontend url in backend

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://nexachat-five.vercel.app",
     credentials: true,
   })
 );
--- backend/utils/socket.js
+++ backend/utils/socket.js
@@ -7,7 +7,7 @@ export const app = express();
 export const server = http.createServer(app);
 
 export const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "https://nexachat-five.vercel.app" },
 });
 
 export const onlineUsers = new Map();

295d2150145295aa9759555156a0f3c36e112edc
Shreshth <shreshth100vishwa@gmail.com>
2025-03-30 19:01:46 +0530

    Fixed case sensitivity issue

--- backend/utils/CustomError.js
+++ backend/utils/CustomError.js
@@ -1,12 +1,9 @@
 class CustomError extends Error {
-
-    constructor(statusCode, message) {
-        super()
-        this.statusCode = statusCode;
-        this.message = message;
-    }
+  constructor(statusCode, message) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+  }
 }
 
-
 export default CustomError;
-
